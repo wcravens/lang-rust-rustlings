af0e91f101dbddd91423d106f0b901872e148d7d
Wes Cravens <wcravens@cortex-it.com>
2024-03-19 18:48:08 -0500

    Play with more rustlings

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,7 +4,8 @@
 
 // I AM NOT DONE
 
-pub fn bigger(a: i32, b: i32) -> i32 {
+pub fn bigger( a: i32, b: i32 ) -> i32 {
+  
     // Complete this function to return the bigger number!
     // Do not use:
     // - another function call
